9b5f1f62b5c9b816f00d419b570b68c5e7e6421a
Hilary Ly <hilaryly@hilarys-mbp.home>
2020-06-12 18:55:30 -0400

    updated flexbox for two columns display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, Highlight, SearchBox, Hits } from 'react-instantsearch-dom';
+import {
+  InstantSearch,
+  Highlight,
+  SearchBox,
+  Hits,
+  Pagination,
+  Configure,
+} from 'react-instantsearch-dom';
 import {
   GoogleMapsLoader,
   GeoSearch,
@@ -13,33 +20,59 @@ import PropTypes from 'prop-types';
 const searchClient = algoliasearch(APP_ID, API_KEY);
 
 const App = () => (
-  <InstantSearch indexName="AEROS" searchClient={searchClient}>
-    <div style={{ height: 500, width: 1000 }}>
-      <GoogleMapsLoader apiKey={MAPS_KEY}>
-        {google => (
-          <GeoSearch
-            google={google}
-            // mapTypeId={google.maps.MapTypeId.SATELLITE}
-          >
-            {({ hits }) => (
-              <div>
-                {hits.map(hit => (
-                  <Marker key={hit.objectID} hit={hit} />
-                ))}
+  <div>
+    <InstantSearch indexName="AEROS" searchClient={searchClient}>
+      <div className="container" style={{ width: 1500 }}>
+        <header className="header">
+          <h1 className="header-title">
+            <a href="/">Airports</a>
+          </h1>
+          <p className="header-subtitle">
+            {' '}
+            <a href="https://github.com/algolia/react-instantsearch">
+              Algolia InstantSearch
+            </a>
+          </p>
+        </header>
+        <div className="map_hits" style={{ height: 500, width: 1500 }}>
+          <div style={{ height: 750, width: 600 }}>
+            <GoogleMapsLoader apiKey={MAPS_KEY}>
+              {google => (
+                <GeoSearch
+                  google={google}
+                  // mapTypeId={google.maps.MapTypeId.SATELLITE}
+                >
+                  {({ hits }) => (
+                    <div>
+                      {hits.map(hit => (
+                        <Marker key={hit.objectID} hit={hit} />
+                      ))}
+                    </div>
+                  )}
+                </GeoSearch>
+              )}
+            </GoogleMapsLoader>
+          </div>
+
+          <div className="search-panel">
+            <div className="search-panel__results" style={{ width: 600 }}>
+              <SearchBox
+                className="searchbox"
+                translations={{
+                  placeholder: '',
+                }}
+              />
+              <Hits hitComponent={Hit} />
+              <Configure hitsPerPage={12} analytics={true} />
+              <div className="pagination">
+                <Pagination />
               </div>
-            )}
-          </GeoSearch>
-        )}
-      </GoogleMapsLoader>
-      <SearchBox
-        className="searchbox"
-        translations={{
-          placeholder: '',
-        }}
-      />
-      <Hits hitComponent={Hit} />
-    </div>
-  </InstantSearch>
+            </div>
+          </div>
+        </div>
+      </div>
+    </InstantSearch>
+  </div>
 );
 
 function Hit(props) {
@@ -65,90 +98,3 @@ Hit.propTypes = {
 };
 
 export default App;
-
-// import {
-//   InstantSearch,
-//   Hits,
-//   SearchBox,
-//   Pagination,
-//   Highlight,
-//   // ClearRefinements,
-//   // RefinementList,
-//   // Configure,
-// } from 'react-instantsearch-dom';
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <header className="header">
-//           <h1 className="header-title">
-//             <a href="/">Airports</a>
-//           </h1>
-//           <p className="header-subtitle">
-//             {' '}
-//             <a href="https://github.com/algolia/react-instantsearch">
-//               Algolia InstantSearch
-//             </a>
-//           </p>
-//         </header>
-
-//         {/* <div style={{ height: 500 }}>
-//           <GoogleMapsLoader apiKey="APIKEY">
-//             {google => (
-//               <GeoSearch google={window.google}>
-//                 {({ hits }) => (
-//                   <div>
-//                     <Control />
-//                     {hits.map(hit => (
-//                       <Marker key={hit.objectID} hit={hit} />
-//                     ))}
-//                   </div>
-//                 )}
-//               </GeoSearch>
-//             )}
-//           </GoogleMapsLoader>
-//         </div> */}
-
-//         <div className="container">
-//           <InstantSearch searchClient={searchClient} indexName="AEROS">
-//             <div className="search-panel">
-//               <div className="search-panel__results">
-//                 <SearchBox
-//                   className="searchbox"
-//                   translations={{
-//                     placeholder: '',
-//                   }}
-//                 />
-//                 <Hits hitComponent={Hit} />
-
-//                 <div className="pagination">
-//                   <Pagination />
-//                 </div>
-//               </div>
-//             </div>
-//           </InstantSearch>
-//         </div>
-//       </div>
-//       <InstantSearch indexName="AEROS" searchClient={searchClient}>
-//     <div style={{ height: 500 }}>
-//       <GoogleMapsLoader apiKey="APIKEY">
-//         {google => (
-//           <GeoSearch
-//             google={google}
-//             mapTypeId={google.maps.MapTypeId.SATELLITE}
-//           >
-//             {({ hits }) => (
-//               <div>
-//                 {hits.map(hit => <Marker key={hit.objectID} hit={hit} />)}
-//               </div>
-//             )}
-//           </GeoSearch>
-//         )}
-//       </GoogleMapsLoader>
-//     </div>
-//   </InstantSearch>
-//     );
-//   }
-// }
